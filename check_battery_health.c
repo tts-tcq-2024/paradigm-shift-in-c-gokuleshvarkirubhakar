f62116983c8aba318e0032f641891968df8e2c35
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-29 09:57:49 +0530

    Update check_battery_health.c

--- check_battery_health.c
+++ check_battery_health.c
@@ -90,21 +90,3 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 
     return status;
 }
-
-// Tests
-int main() {
-    assert(!batteryIsOk(-5.0, 70, 0.7));    	// Low temperature
-    assert(batteryIsOk(0, 70, 0.7));		// Boundary temperature - Lower
-    assert(batteryIsOk(45, 70, 0.7));    	// Boundary temperature - Upper
-    assert(!batteryIsOk(50, 70, 0.7));      	// High temperature
-    assert(!batteryIsOk(25, 10, 0.7));      	// Low SOC
-    assert(batteryIsOk(25, 20, 0.7));    	// Boundary SOC - Lower
-    assert(batteryIsOk(25, 80, 0.7));    	// Boundary SOC - Upper
-    assert(!batteryIsOk(25, 90, 0.7));     	// High SOC
-    assert(!batteryIsOk(25, 70, -0.1));     	// Low charge rate
-    assert(batteryIsOk(25, 70, 0));         	// Boundary CR - Lower
-    assert(batteryIsOk(25, 70, 0.8));       	// Boundary CR - Upper
-    assert(!batteryIsOk(25, 70, 0.9));      	// High charge rate 
-    assert(!batteryIsOk(50, 90, 0.9));      	// All parameters out of range
-    assert(batteryIsOk(25, 70, 0.7));       	// Normal parameters
-}
